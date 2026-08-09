e9c1a6f894bba9da2df7c7594f62b83b33f834cb
Admantix <williamwijaya499@gmail.com>
2026-08-09 22:12:17 +0700

    feat: adjust upload weights for segformer only

--- deployment/upload/upload_weights.py
+++ deployment/upload/upload_weights.py
@@ -3,18 +3,22 @@ from huggingface_hub import HfApi
 
 MODEL_DIR = Path(__file__).resolve().parents[2] / "model"
 
-UPLOAD = {
-    "segformer": ["*.pth"] ,
-    "unet": []
-}
-
 api = HfApi()
 api.create_repo("adamantix/pdisnet-weights", repo_type="model", exist_ok=True)
 
-for name, skip in UPLOAD.items():
-    api.upload_folder(
-        folder_path = MODEL_DIR/name,
-        path_in_repo = name,
-        repo_id = "adamantix/pdisnet-weights",
-        ignore_patterns = skip,
-    )
+api.upload_folder(
+    folder_path = MODEL_DIR / "segformer",
+    path_in_repo = "segformer",
+    repo_id = "adamantix/pdisnet-weights",
+    ignore_patterns = ["*.pth"],
+)
+
+'''
+# for name, skip in UPLOAD.items():
+#     api.upload_folder(
+#         folder_path = MODEL_DIR/name,
+#         path_in_repo = name,
+#         repo_id = "adamantix/pdisnet-weights",
+#         ignore_patterns = skip,
+#     )
+'''
